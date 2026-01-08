41654e173499f8b7a0ac923cd77385403e2f7d09
Sophia Pouya <shp77@cornell.edu>
2026-01-08 13:44:03 -0600

    updating baseline for new work

---
+++ utils/inspect_npy.py
@@ -0,0 +1,6 @@
+import os
+import numpy as np
+
+file_1 = "/Users/sophiapouya/workspace/bcm/CATDI/Results/model/DBSTRD001_dicts.npz"
+file = np.load(file_1)
+print("loaded")
---
+++ utils/inspect_npz.py
@@ -0,0 +1,3 @@
+import os
+
+file_1 = "/Users/sophiapouya/workspace/bcm/CATDI/Results/model/DBSTRD001_dicts.npz"
