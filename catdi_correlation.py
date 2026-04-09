ae7a8775f03ae470372bcbaec6c45fc7bc09bd3b
Sophia Pouya <shp77@cornell.edu>
2026-04-09 16:54:32 -0500

    most recent catdi correation script

--- catdi_correlation.py
+++ catdi_correlation.py
@@ -4,18 +4,34 @@ import numpy as np
 from scipy.stats import pearsonr
 import seaborn as sns
 import matplotlib.pyplot as plt
+from statsmodels.stats.multitest import fdrcorrection
 
-
-# subj_name = "DBSTRD001"
-# ref_type = "bipolar_alternating"
+ELEC_TYPE = "DBS"  # either DBS or SEEG
 
 catdi_scores_excel = "/Users/sophiapouya/workspace/bcm/CATDI/CATDI_scores.xlsx"
-all_subjs = ["DBSTRD001","DBSTRD002","DBSTRD006","DBSTRD008","DBSTRD010","DBSTRD014"]
-ref_types = ["bipolar","car","esr","bipolar_alternating"]
-base_dir = "/Users/sophiapouya/workspace/bcm/CATDI/neuralData/dbsData/"
-plot_dir = "/Users/sophiapouya/workspace/bcm/CATDI/depression-severity-dbs/corr_plots"
+all_subjs = ["DBSTRD001","DBSTRD002","DBSTRD006","DBSTRD008","DBSTRD010","DBSTRD011","DBSTRD014"]
+
+if ELEC_TYPE == "SEEG":
+    ref_types = ["bipolar"]
+    base_dir = "/Users/sophiapouya/workspace/bcm/CATDI/neuralData/seegData/"
+    plot_dir = "/Users/sophiapouya/workspace/bcm/CATDI/depression-severity-dbs/corr_plots_seeg"
+    os.makedirs(plot_dir,exist_ok=True)
+else: 
+    ref_types = ["bipolar_alternating"]
+    base_dir = "/Users/sophiapouya/workspace/bcm/CATDI/neuralData/dbsData/"
+    plot_dir = "/Users/sophiapouya/workspace/bcm/CATDI/depression-severity-dbs/corr_plots_dbs"
+    os.makedirs(plot_dir, exist_ok=True)
 
 for subj_name in all_subjs: 
+
+    if ELEC_TYPE == "SEEG":
+        metadata_file = os.path.join(base_dir, subj_name, f"{subj_name}_metadata.csv")
+        # csv with columns channel_name and channel_region
+        metadata_df = pd.read_csv(metadata_file)
+       
+        # ensure the name is consistent with power data file name
+        metadata_df["ch_name"] = metadata_df["channel_name"]
+
     for ref_type in ref_types: 
         sbj_dir = os.path.join(base_dir, subj_name)
         if ref_type == "bipolar":
@@ -49,7 +65,12 @@ for subj_name in all_subjs:
         catdi_excel["session"] = catdi_excel["session"].astype(str)
 
         subj_data = pd.merge(subj_data, catdi_excel[["session", "Result"]], how="left", on="session")
-
+        
+        if ELEC_TYPE == "SEEG":
+            subj_data = pd.merge(subj_data, metadata_df, how="left", on="ch_name")
+            # create the channel mapping 
+            channel_region_map = dict(zip(subj_data["ch_name"], subj_data["channel_region"]))
+        
         # rename the column 
         subj_data = subj_data.rename(columns={"Result":"score"})
 
@@ -57,7 +78,7 @@ for subj_name in all_subjs:
         bands = ['delta', 'theta', 'alpha', 'beta', 'low_gamma', 'high_gamma']
 
         # Use LaTeX labels for the plot
-        band_labels = ['$\delta$', '$\\theta$', '$\\alpha$', '$\\beta$', '$\gamma$', 'h$\gamma$']
+        band_labels = [r'$\delta$', r'$\theta$', r'$\alpha$', r'$\beta$', r'$\gamma$', r'h$\gamma$']
 
         plot_data = {}
         p_val_data = {}
@@ -71,7 +92,6 @@ for subj_name in all_subjs:
             p_values = []
 
             for band in bands:
-
                 # correlation between the power band and the merged score
                 r, p = pearsonr(contact_df[band], contact_df['score'])
                 r_values.append(r)
@@ -84,13 +104,37 @@ for subj_name in all_subjs:
         plot_df = pd.DataFrame(plot_data, index=band_labels).sort_index(axis=1)
         p_val_df = pd.DataFrame(p_val_data, index=band_labels).sort_index(axis=1)
 
+        if ELEC_TYPE == "SEEG":
+            sorted_chans = sorted(
+                plot_df.columns, 
+                key= lambda ch: channel_region_map[ch]
+            )
+            plot_df = plot_df[sorted_chans]
+            p_val_df = p_val_df[sorted_chans]
+
+        # fdr correction for the pvals
+        p_vals_flat = p_val_df.values.flatten()
+        rejected, correct_p_vals=fdrcorrection(p_vals_flat)
+        p_val_df_corrected = pd.DataFrame(
+            correct_p_vals.reshape(p_val_df.shape),
+            index=p_val_df.index,
+            columns=p_val_df.columns
+        )
+
         # annotations for significant p values
-        annotations = p_val_df.map(lambda p: '*' if p < 0.05 else '')
+        annotations = p_val_df_corrected.map(lambda p: '*' if p < 0.05 else '')
 
         # plot
         plt.figure(figsize=(12, 8))
-        ax = sns.heatmap(data=plot_df, cmap='RdBu_r', center=0, vmin=-1, vmax=1, annot=annotations, fmt='')
-        ax.set_xticklabels(ax.get_xticklabels(), fontsize=16)
+
+
+        if ELEC_TYPE == "SEEG":
+            region_labels = [channel_region_map[ch].upper() for ch in plot_df.columns]
+            ax = sns.heatmap(data=plot_df, cmap='RdBu_r', center=0, vmin=-1, vmax=1, annot=annotations, annot_kws={"color": "black", "fontsize":14}, fmt='',  xticklabels=region_labels)
+        else:
+            ax = sns.heatmap(data=plot_df, cmap='RdBu_r', center=0, vmin=-1, vmax=1, annot=annotations, annot_kws={"color": "black", "fontsize":14}, fmt='')
+            ax.set_xticklabels(ax.get_xticklabels(), fontsize=16)
+
         ax.set_yticklabels(ax.get_yticklabels(), fontsize=20)
 
         plt.title(f"Correlation: {subj_name} ({ref_type})",fontsize=20)
@@ -99,7 +143,7 @@ for subj_name in all_subjs:
         plt.tight_layout()
 
         # save and show
-        save_path = os.path.join(plot_dir, f"{subj_name}_{ref_type}_correlation_segmented_sections.png")
+        save_path = os.path.join(plot_dir, f"{subj_name}_{ref_type}.png")
         plt.savefig(save_path)
         plt.close()
 
